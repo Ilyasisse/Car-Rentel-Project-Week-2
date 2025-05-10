0ea104efc8fc0ee9c3e09e075f4b86bd4d77fa8a
Ilyas Isse <Ilyasisse574@gmail.com>
2025-05-09 22:17:15 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+function openMenu() {
+  const body = document.body;
+
+  document.body.classList.add("open");
+}
+
+function closeMenu() {
+  const body = document.body;
+
+  document.body.classList.remove("open");
+}
